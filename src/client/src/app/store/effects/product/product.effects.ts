f3b08401193c6d5297df36af0b0b0a77f0ad5053
mekdes45 <mekmis2020@gmail.com>
2021-11-11 19:08:13 -0800

    feat(store):add action,effect,resducer,and select product feature

---
+++ src/client/src/app/store/effects/product/product.effects.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { mergeMap, map, catchError } from 'rxjs/operators';
+import { ProductService } from 'src/app/services/product.service';
+import { loadProducts, loadProductsSuccess, loadProductsFailure, createProduct, createProductSuccess, createProductFailure } from '../../actions/product/product.actions';
+
+
+
+@Injectable()
+export class ProductEffects {
+
+
+  loadProducts$ = createEffect(() =>
+  this.actions$.pipe(
+    ofType(loadProducts),
+    mergeMap(() =>
+      this.productService.getProducts().pipe(
+        map((data) => loadProductsSuccess({ data })),
+        catchError((error) => of(loadProductsFailure({ error })))
+      )
+    )
+  )
+  );
+  
+  createProduct$ = createEffect(() =>
+  this.actions$.pipe(
+    ofType(createProduct),
+    mergeMap((action) =>
+      this.productService.createProduct(action.data).pipe(
+        map((data) => createProductSuccess({ data })),
+        catchError((error) => of(createProductFailure({ error })))
+      )
+    )
+  )
+);
+
+
+  constructor(private actions$: Actions, private productService: ProductService) {}
+
+}
--- src/client/src/app/store/index.ts
+++ src/client/src/app/store/index.ts
@@ -7,16 +7,19 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import * as fromUser from './reducers/user/user.reducer';
+import * as fromProduct from './product/reducers/product.reducer';
 
 
 export interface AppState {
 
   [fromUser.userFeatureKey]: fromUser.State;
+  [fromProduct.productFeatureKey]: fromProduct.State;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
 
   [fromUser.userFeatureKey]: fromUser.reducer,
+  [fromProduct.productFeatureKey]: fromProduct.reducer,
 };
 
 
